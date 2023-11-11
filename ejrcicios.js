4fe1833bac2f03b4f9caf02ea84438cf14ba86a4
jfscalante <scalante2002@gmail.com>
2023-11-11 08:30:46 -0500

    actualice unos ejercicios

--- ejrcicios.js
+++ ejrcicios.js
@@ -123,3 +123,145 @@ if (ususario === 'admin'){
 }else {
     alert('el usuario no esta registrado');
 }
+
+//Décimo//
+let camisas = prompt("Ingrese el total de camisas compradas")
+
+if (camisas >= 3){
+    alert("Se ha aplicado un descuento de un 20%")
+} else {
+    alert("Se ha aplicado un 10% de descuento en su compra")
+};
+
+
+//Undécimo//
+let numA = parseFloat(prompt("Digite un número"));
+let numB = parseFloat(prompt("Digite el segundo número"));
+
+if (numA === numB){
+    alert("Los números se multiplican. El resultado es " + (numA * numB))
+} else if (numA > numB){
+    alert("En este caso se restan. El resultado es " + (numA - numB))
+} else {
+    alert("Se suman. El resultado es " + (numA + numB))
+};
+
+
+//Duodécimo//
+let ope = prompt("Bienvenido a la calculadora básica. ¿Qué desea hacer (suma, resta, multiplicación o división)");
+let num1 = parseFloat(prompt("Ingrese el número 1"));
+let num2 = parseFloat(prompt("Ingrese el número 2"));
+
+if (ope === "Suma" || ope === "suma" || ope === "SUMA"){
+    alert("El resultado de la suma es " + (num1 + num2))
+} else if (ope === "Resta" || ope === "resta" || ope === "RESTA"){
+    alert("El resultado de la resta es " + (num1 - num2))
+} else if (ope === "Multiplicacion" || ope === "multiplicacion" || ope === "MULTIPLICACION"){
+    alert("El resultado de la multiplicación es " + (num1 * num2))
+} else if (ope === "Division" || ope === "division" || ope === "DIVISION"){
+    alert("El resultado de la division es " + (num1 / num2))
+} else {
+    alert("Error. Ingrese una operación permitida/válida")
+};
+
+
+//Decimotercero//
+let numIn = parseFloat(prompt("Ingrese un número"))
+
+if (numIn % 2 === 0){
+    alert("El número es par")
+} else {
+    alert("El número es impar")
+};
+
+
+//Decimocuarto//
+alert("Calcularemos el IMC (índice de masa corporal)");
+let pesokg = parseFloat(prompt("Ingrese su peso (Kg)"));
+let estatura = parseFloat(prompt("Ingrese su estatura"));
+
+let imc = pesokg / (estatura * estatura)
+
+if (imc < 18.5){
+    alert("Su IMC es " + imc + " y se encuentra bajo de peso.")
+} else if (imc > 18.5 && imc < 24.9){
+    alert("Su IMC es " + imc + " y su estado es saludable")
+} else if (imc > 25 && imc < 29.9){
+    alert("Su IMC es " + imc + " y se encuentra en sobrepeso")
+} else if (imc >= 30){
+    alert("Su IMC es " + imc + " y se encuentra en estado de obesidad")
+} else {
+    alert("Error")
+};
+
+
+//Decimoquinto//
+let numVe = parseFloat(prompt("Ingrese un número"));
+
+if (numVe > 0){
+    alert("El número es positivo")
+} else if (numVe < 0){
+    alert("El número es negativo")
+} else if (numVe === 0){
+    alert("El número es igual a 0")
+} else {
+    alert("Error. Ingrese un número válido")
+};
+
+
+//Decimosexto//
+let nota = parseFloat(prompt("Ingrese la nota de Algoritmos"));
+
+if (nota > 1.0 && nota < 3.3){
+    alert("Su desempeño es BAJO")
+} else if (nota > 3.4 && nota < 3.8){
+    alert("Su desempeño es BASICO")
+} else if (nota > 3.9 && nota < 4.4){
+    alert("Su desempeño es ALTO")
+} else if (nota > 4.5 && nota < 5.0){
+    alert("Su desempeño es SUPERIOR")
+} else {
+    alert("Error. Ingrese una nota válida")
+};
+
+
+//Decimoséptimo//
+let fig = prompt("Ingrese la figura a la que le desea calcular el área");
+
+if (fig === "triangulo" || fig === "Triangulo" || fig === "TRIANGULO"){
+	let base = parseFloat(prompt("Ingrese el valor de la base (cm)"))
+	let altura = parseFloat(prompt("Ingrese el valor de la altura (cm)"))
+	
+	alert("El área del triángulo es " + ( (base * altura) / 2) )
+} else if (fig === "cuadrado" || fig === "Cuadrado" || fig === "CUADRADO"){
+	let lado1 = parseFloat(prompt("Ingrese el valor del lado uno (cm)")) 
+	let lado2 = parseFloat(prompt("Ingrese el valor del lado dos (cm)"))
+	
+	alert("El área del cuadrado es " + (lado1 * lado2))
+} else {
+	alert("Error. Ingrese una figura permitida. No se permiten tildes")
+};
+
+
+//Decimoctavo//
+let numEs = prompt("Ingrese un número cualquiera");
+
+if (numEs < 10){
+	alert("Es un número menor que 10")
+} else if (numEs >= 10 && numEs <= 20){
+	alert("Es un número entre 10 y 20")
+} else if (numEs > 20){
+	alert("Es un número mayor que 20")
+} else {
+	alert("Error. Ingrese un número válido")
+};
+
+//Vigésimo//
+let genero = prompt("Ingrese su género"); 
+let edad = prompt("Ingrese su edad");
+
+if (genero === "masculino" || genero === "Masculino" && edad >= 18){
+	alert("Usted es elegible para el servicio militar");
+} else {
+	alert("Usted no es elegible para prestar el servicio militar")
+};
